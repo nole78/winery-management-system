1b78608d6a03e3e4f0b74fd870fa1650e1e8283a
Anja Srećković <anja.sreckovic24@gmail.com>
2026-01-15 22:33:58 +0100

    Entitet Vinova loza dodat

---
+++ ERS_Projekat_Template/Domain/Modeli/VinovaLoza.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Enumeracije;
+
+namespace Domain.Modeli
+{
+    public class VinovaLoza
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Naziv { get; set; } = string.Empty;
+        public float NivoSecera { get; set; } = 0;
+        public int GodSadnje { get; set; } = 0;
+        public string RegionUzgoja { get; set; } = string.Empty;
+        public FazaZrelosti Zrelost { get; set; }
+
+        public VinovaLoza()
+        {
+            Id= Guid.NewGuid().ToString();
+        }
+
+        public VinovaLoza(string naziv, float nivoSecera, int godSadnje, string regionUzgoja, FazaZrelosti zrelost)
+        {
+            Id= Guid.NewGuid().ToString();
+            Naziv=naziv;
+            NivoSecera=nivoSecera;
+            GodSadnje=godSadnje;
+            RegionUzgoja=regionUzgoja;
+            Zrelost=zrelost;
+        }
+    }
+}
