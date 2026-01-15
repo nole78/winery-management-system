a649f4ea7a447d80fd158e36b5a408cc969b949d
Nole <daki.nole@gmail.com>
2026-01-15 12:08:46 +0100

    Dodat entitet VinskiPodrum

---
+++ ERS_Projekat_Template/Domain/Modeli/VinskiPordum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Modeli
+{
+    public class VinskiPordum
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Naziv { get; set; } = string.Empty;
+        public int Temperatura { get; set; } = 0;
+        public int MaxPaleta { get; set; } = 0;
+        public VinskiPordum() { }
+        public VinskiPordum(string naziv, int temperatura, int maxPaleta)
+        {
+            Id = (Guid.NewGuid().ToString()).Substring(0,8);
+            Naziv = naziv;
+            Temperatura = temperatura;
+            MaxPaleta = maxPaleta;
+        }
+    }
+}
