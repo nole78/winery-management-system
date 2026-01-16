e00b0c6fed34cd53bb490200ff8d10a634debeac
Petra Janjic <petra.janjic@gmail.com>
2026-01-16 13:36:34 +0100

    Dodat repozitorijum za lozu

---
+++ ERS_Projekat_Template/Domain/Repozitorijumi/ILozaRepozitorijum.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Enumeracije;
+using Domain.Modeli;
+
+namespace Domain.Repozitorijumi
+{
+    public interface ILozaRepozitorijum
+    {
+        public VinovaLoza PosadiLozu(VinovaLoza loza);
+        public bool PromeniNivoSecera(VinovaLoza loza, int procenat);
+        public bool OberiLozu(VinovaLoza loza, int kolicina);
+        public IEnumerable<VinovaLoza> PrikaziSveLoze();
+        public IEnumerable<VinovaLoza> PrikaziPoNazivu(string naziv);
+        public IEnumerable<VinovaLoza> PrikaziPoID(string id);
+        public IEnumerable<VinovaLoza> PrikaziPoNivouSecera(float nivo);
+        public IEnumerable<VinovaLoza> PrikaziPoGodini(int godina);
+        public IEnumerable<VinovaLoza> PrikaziPoRegionu(string region);
+        public IEnumerable<VinovaLoza> PrikaziPoZrelosti(FazaZrelosti faza);
+
+    }
+}
+
+
