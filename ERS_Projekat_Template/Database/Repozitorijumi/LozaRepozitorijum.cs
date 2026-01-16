659e5f9a9bbbcd671a38622fb2c17c80700b9f78
Petra Janjic <petra.janjic@gmail.com>
2026-01-16 19:19:01 +0100

    Dodat repozitorijum za lozu

---
+++ ERS_Projekat_Template/Database/Repozitorijumi/LozaRepozitorijum.cs
@@ -0,0 +1,98 @@
+﻿using Domain.BazaPodataka;
+using Domain.Modeli;
+using Domain.Repozitorijumi;
+
+namespace Database.Repozitorijumi
+{
+    public class LozaRepozitorijum : ILozaRepozitorijum
+    {
+        IBazaPodataka BazaPodataka;
+
+        public LozaRepozitorijum(IBazaPodataka bazaPodataka)
+        {
+            BazaPodataka = bazaPodataka;
+        }
+
+        public VinovaLoza DodajLozu(VinovaLoza loza)
+        {
+            try
+            {
+                VinovaLoza postoji = PronadjiLozuPoID(loza.Id);
+
+                if (postoji.Id == string.Empty)
+                {
+                    BazaPodataka.Tabele.VinoveLoze.Add(loza);
+                    BazaPodataka.SacuvajPromene();
+
+                    return loza;
+                }
+
+                return new VinovaLoza();
+            }
+            catch { return new VinovaLoza(); }
+
+        }
+
+        public bool AzurirajLozu(VinovaLoza loza)
+        {
+            try
+            {
+                for (int i = 0; i < BazaPodataka.Tabele.VinoveLoze.Count; i++)
+                {
+                    if (BazaPodataka.Tabele.VinoveLoze[i].Id == loza.Id)
+                    {
+                        BazaPodataka.Tabele.VinoveLoze[i] = loza;
+                        return BazaPodataka.SacuvajPromene();
+                    }
+                }
+
+                return false;
+            }
+            catch { return false; }
+            
+        }
+
+        public VinovaLoza PronadjiLozuPoID(string id)
+        {
+            try
+            {
+                foreach (VinovaLoza vl in BazaPodataka.Tabele.VinoveLoze)
+                {
+                    if (vl.Id == id) return vl;
+                }
+
+                return new VinovaLoza();
+            }
+            catch { return new VinovaLoza(); }
+        }
+
+        public IEnumerable<VinovaLoza> PregledLozaPoSeceru(float nivoSecera)
+        {
+            List<VinovaLoza> lista = new List<VinovaLoza>();
+
+            try
+            {
+                foreach (VinovaLoza vl in BazaPodataka.Tabele.VinoveLoze)
+                {
+                    if (vl.NivoSecera == nivoSecera) lista.Add(vl);
+                }
+
+                return lista;
+            }
+            catch { return lista; }
+        }
+
+        public IEnumerable<VinovaLoza> PregledSvihLoza()
+        {
+            List<VinovaLoza> lista = new List<VinovaLoza>();
+
+            try
+            {
+                foreach (VinovaLoza vl in BazaPodataka.Tabele.VinoveLoze) lista.Add(vl);
+                return lista;
+            }
+            catch { return lista; }
+        }
+
+    } 
+}
